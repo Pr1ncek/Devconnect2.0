4d6fe3f808e2e980108f1c7a26d1790358339161
Prince K <k.prince1997@gmail.com>
2018-11-20 22:54:09 -0800

    Finished logic for signup input validation

--- validation/register.js
+++ validation/register.js
@@ -4,10 +4,42 @@ const isEmpty = require('./is-empty');
 const validateSignupInput = data => {
   const errors = {};
 
-  const { name, email, password, confirmPassword } = data;
+  let { name, email, password, confirmPassword } = data;
+  name = isEmpty(name) ? '' : name;
+  email = isEmpty(email) ? '' : email;
+  password = isEmpty(password) ? '' : password;
+  confirmPassword = isEmpty(confirmPassword) ? '' : confirmPassword;
 
   if (!validator.isLength(name, { min: 2, max: 25 })) {
     errors.name = 'Name must be between 2 and 25 characters';
+  }
+
+  if (validator.isEmpty(name)) {
+    errors.name = 'Name is required';
+  }
+
+  if (validator.isEmpty(email)) {
+    errors.email = 'Email is required';
+  }
+
+  if (!validator.isEmail(email)) {
+    errors.email = 'Invalid email';
+  }
+
+  if (validator.isEmpty(password)) {
+    errors.password = 'Password is required';
+  }
+
+  if (!validator.isLength(password, { min: 5, max: 25 })) {
+    errors.password = 'Password must be between 2 and 25 characters';
+  }
+
+  if (confirmPassword !== password) {
+    errors.confirmPassword = 'Password does not match';
+  }
+
+  if (validator.isEmpty(confirmPassword)) {
+    errors.confirmPassword = 'Confirm password';
   }
 
   return {
@@ -16,4 +48,8 @@ const validateSignupInput = data => {
   };
 };
 
+const x = e => {
+  return validator.isEmail(e);
+};
+console.log(x('prince'));
 module.exports = validateSignupInput;
