eaf971a279e7c8e4867966c10d5d311d54249a26
Prince K <k.prince1997@gmail.com>
2018-11-26 18:59:02 -0800

    Created a react app and set up navbar and landing components

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className="bg-dark text-white pt-3 text-center font-weight-bold">
+        Copyright &copy; 2018 Dev Connector
+      </footer>
+    </div>
+  );
+}
